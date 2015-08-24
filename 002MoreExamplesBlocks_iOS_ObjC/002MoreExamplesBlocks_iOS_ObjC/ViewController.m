0db5f1d3a3cd99a7b327b7157dfdc7cf1556d84e
Henry AT <henry.ambicho@mercadolibre.cl>
2015-08-24 18:45:11 -0300

    Add more examples for have a consider

--- 002MoreExamplesBlocks_iOS_ObjC/002MoreExamplesBlocks_iOS_ObjC/ViewController.m
+++ 002MoreExamplesBlocks_iOS_ObjC/002MoreExamplesBlocks_iOS_ObjC/ViewController.m
@@ -76,14 +76,147 @@
 }
 
 - (IBAction)btn04:(id)sender {
-    
+    NSArray *colors = [NSArray arrayWithObjects:@"Red",@"Green",@"Blue", nil];
+    // Enumerating usign block
+    [colors enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"%@ at %lu", obj, idx);
+        if (idx == 1) {
+            NSLog(@"Found index 1, stopping");
+            *stop = YES;
+        }
+    } // End of Block
+     ];
+    //  - - - - - -  -
+    for (id obj in colors) {
+        NSUInteger idx = [colors indexOfObject:obj];
+        NSLog(@"%@ at %lu", obj, idx);
+        
+        if (idx == 1) {
+            NSLog(@"Found index 1, stopping");
+            break;
+        }
+    }
     
 }
 
 - (IBAction)btn05:(id)sender {
+    // You can also use them to filter objects based on the code in the block:
+    NSMutableArray* streets = [NSMutableArray array];
+    [streets addObject:@"De Anza Blvd"];
+    [streets addObject:@"Homestead Rd"];
+    [streets addObject:@"Sunnyvale Saratoga Rd"];
+    [streets addObject:@"Fremont Ave"];
+    [streets addObject:@"Stelling Rd"];
+    NSLog( @"All streets: %@", streets );
+    NSIndexSet* roadsIndexes;
+    roadsIndexes = [streets indexesOfObjectsPassingTest:
+                    ^( id obj, NSUInteger idx, BOOL *stop ) {
+                        return [obj hasSuffix:@"Rd"];
+                    }];
+    NSLog( @"roadIndexes: %lu", roadsIndexes.count );
+    NSArray* roads = [streets objectsAtIndexes:roadsIndexes];
+    NSLog( @"roads: %@", roads );
+    
+}
+
+- (IBAction)btn06:(id)sender {
+    
+//    NSDictionary supports both fast enumeration and block enumeration:
+    NSString* const CBCityKey = @"city";
+    NSString* const CBStateKey = @"state";
+    NSString* const CBZipcodeKey = @"zipcode";
+    NSDictionary* info;
+    info = [NSDictionary dictionaryWithObjectsAndKeys:
+            @"Sunnyvale", CBCityKey,
+            @"California", CBStateKey,
+            @"94086", CBZipcodeKey,
+            nil];
+    
+    for ( id key in info ) {
+        NSLog( @"%@: %@", key, [info objectForKey:key] );
+    }
+    
+    [info enumerateKeysAndObjectsUsingBlock:
+     ^( id key, id obj, BOOL *stop ) {
+         NSLog( @"%@: %@", key, obj );
+     }
+     ];
+    
+}
+
+
+- (IBAction)btn07:(id)sender {
+    //    NSSet
+
+        // manually created set.
+    NSSet* days;
+    days = [NSSet setWithObjects: @"Friday", @"Saturday",nil];
+    days = [days setByAddingObject:@"Sunday"];
+    NSLog( @"days: %@", days );
+    // set from array.
+    NSMutableArray* planets = [NSMutableArray array];
+    [planets addObject:@"Earth"];
+    [planets addObject:@"Mercury"];
+    [planets addObject:@"Mars"];
+    [planets addObject:@"Jupiter"];
+    [planets addObject:@"Jupiter"];
+    [planets addObject:@"Jupiter"];
+    NSSet* planetsSet = [NSSet setWithArray:planets];
+    NSLog( @"planetsSet: %@", planetsSet );
+    // add an object.
+    NSSet* morePlanets = [planetsSet setByAddingObject:@"Venus"];
+    if ([planetsSet isSubsetOfSet:morePlanets])
+        NSLog( @"morePlanets is a superset of planetsSet" );
+    // loop through with fast enumeration.
+    for ( id planet in planetsSet ) {
+        if ([planet isEqual:@"Earth"]) {
+            NSLog ( @"Found home!" );
+        }
+    }
+    // filter objects with a block.
+    NSSet* mPlanets = [morePlanets objectsPassingTest:
+                       ^(id obj, BOOL *stop) {
+                           return [obj hasPrefix:@"M"];
+                       }];
+    
+    NSLog( @"mPlanets: %@", mPlanets );
+    
+}
+
+- (IBAction)btn08:(id)sender {
+    
     
     
 }
+
+- (IBAction)btn069:(id)sender {
+    
+    
+    
+    
+}
+
+- (IBAction)btn10:(id)sender {
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
